3382630724443e0cf52c94cfcc9fc5ebffb35588
Tony Field <tony@tonyfield.ca>
2021-02-05 10:16:55 -0500

    Demo request translation

--- sources/my-app/src/ComponentByPathAndPreferredLocale.js
+++ sources/my-app/src/ComponentByPathAndPreferredLocale.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ComponentByPath from "./ComponentByPath";
+import {Typography} from "@material-ui/core";
 
 function ComponentByPathAndPreferredLocale(props) {
   const {
@@ -9,7 +10,17 @@ function ComponentByPathAndPreferredLocale(props) {
 // /site/components/en/left-rails/left-rail-with-no-articles.xml
   if (!path) return null;
   if (!path.startsWith("/site/components/")) return null;
-  return <ComponentByPath path={path.substring(0,17) + locale + path.substring(19)} />
+  const overridePath = path.substring(0,17) + locale + path.substring(19)
+  if (overridePath !== path) {
+    return <>
+      <Typography variant={"h5"} color={"primary"}>
+        App overriding wired relationship to seek locale <em><u>{locale}</u></em> for {path}
+      </Typography>
+      <ComponentByPath path={overridePath} />
+    </>
+  } else {
+    return <ComponentByPath path={path} />
+  }
 }
 
 export default ComponentByPathAndPreferredLocale
